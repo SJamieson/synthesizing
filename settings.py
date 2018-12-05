7e76a47f5f6b0e61150602ff7aa6afdaca2eff4e
Stewart Jamieson <sjamieson@whoi.edu>
2018-12-04 21:01:12 -0500

    Fixed type in settings.py

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ gpu = True
 
 cornet_s_weights = "nets/CORnet-S/Sequential.caffemodel"
 cornet_s_definition = "nets/CORnet-S/Sequential.prototxt"
-cornet_z_weights = "nets/CORnet-S/Sequential.caffemodel"
-cornet_z_definition = "nets/CORnet-S/Sequential.prototxt"
+cornet_z_weights = "nets/CORnet-Z/Sequential.caffemodel"
+cornet_z_definition = "nets/CORnet-Z/Sequential.prototxt"
 caffenet_weights = "nets/caffenet/bvlc_reference_caffenet.caffemodel"
 caffenet_definition = "nets/caffenet/caffenet.prototxt"
 
@@ -13,8 +13,8 @@ caffenet_definition = "nets/caffenet/caffenet.prototxt"
 # These settings should work by default
 # DNN being visualized
 # These two settings are default, and can be overriden in the act_max.py
-net_weights = cornet_s_weights
-net_definition = cornet_s_definition
+net_weights = cornet_z_weights
+net_definition = cornet_z_definition
 
 # Generator DNN
 generator_weights = "nets/upconv/fc6/generator.caffemodel"
